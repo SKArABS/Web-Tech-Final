ba3aed37e1fd09469c488c7dd55094c4edc09477
SKArABS <123462279+SKArABS@users.noreply.github.com>
2024-04-13 17:38:16 +0000

    Update login_action.php
    Commented out line 23

--- action/login_action.php
+++ action/login_action.php
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         // Verify hashed password
         if (password_verify($password, $user['password'])) {
             // Password matches, set session variables
-            $_SESSION['user_id'] = $user['id'];
+            //$_SESSION['user_id'] = $user['id'];
             $_SESSION['role'] = $user['role'];
 
             // Redirect to the appropriate dashboard based on user's role
